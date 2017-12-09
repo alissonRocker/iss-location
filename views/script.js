18169c9a19a9daf28295735ff6994fe32fa68487
ALISSON <alissonvsouza02@gmail.com>
2017-12-09 08:39:46 -0200

    Control
    Recuperando dados da iss e mostrando no google maps

---
+++ views/script.js
@@ -0,0 +1,34 @@
+function initMap() {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        scrollwheel: false,
+        zoom: 3
+    });
+
+    var iss = new google.maps.Marker({
+        map: map,
+        icon: 'img/iss.png',
+        position: {lat: 0, lng: 0},
+    });
+
+    // Search location iss
+    var search = setInterval(function() {
+        
+        $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
+
+            if(data.message == "success") {
+                var _lat = data.iss_position.latitude;
+                var _lng = data.iss_position.longitude;
+
+                var LatLng = new google.maps.LatLng(_lat, _lng);
+                iss.setPosition(LatLng);
+                map.panTo(LatLng);
+            }
+
+        });
+
+    }, 3000);
+
+}
